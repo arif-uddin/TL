85989d995e44b3fea6a92a55ef1ea75e230c8d97
arif-uddin <arif.uddin.cse@gmail.com>
2018-10-18 11:41:56 +0600

    Sign Up Layout complete

--- app/src/main/java/com/lazydevs/tinylens/Activity/RegistrationActivity.java
+++ app/src/main/java/com/lazydevs/tinylens/Activity/RegistrationActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,7 +26,7 @@ import com.lazydevs.tinylens.R;
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText FirstName, LastName, Email, PassWord;
-    Button Register;
+    ImageButton Register;
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
     FirebaseStorage firebaseStorage;
