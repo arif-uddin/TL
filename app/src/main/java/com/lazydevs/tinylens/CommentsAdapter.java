2056f3190d2ead1440448e23e2525c16764dfce7
shakibuzzaman3104 <shakibuzzaman3104@gmail.com>
2018-10-31 21:10:43 +0600

    Comment Bug Solved

--- app/src/main/java/com/lazydevs/tinylens/CommentsAdapter.java
+++ app/src/main/java/com/lazydevs/tinylens/CommentsAdapter.java
@@ -23,7 +23,7 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.ViewHo
     ArrayList<ModelUser> users;
     
 
-    public CommentsAdapter(Context context, ArrayList<ModelComment> comments, ArrayList<ModelUser> users, PostDetailViewActivity postDetailViewActivity) {
+    public CommentsAdapter(Context context, ArrayList<ModelComment> comments, ArrayList<ModelUser> users) {
         this.context = context;
         this.comments = comments;
         this.users = users;
